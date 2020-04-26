b0ccb6cd33b1a6a529601e08709a2b4d0a816866
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-25 20:52:41 -0500

    Project created with Spring Boot Framework

---
+++ src/test/java/co/edu/javeriana/bootnode/bootnode/BootnodeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.bootnode.bootnode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootnodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
