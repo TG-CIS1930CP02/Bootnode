34ee23a55806ee473a6cc0cef04ad9f1aca8d401
JhonnyParraB <jhonnyparra01@hotmail.com>
2020-04-25 22:27:21 -0500

    Refactor services and controller

--- src/main/java/co/edu/javeriana/bootnode/restservices/NodeController.java
+++ src/main/java/co/edu/javeriana/bootnode/restservices/NodeController.java
@@ -13,28 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.javeriana.bootnode.entitys.Node;
-import co.edu.javeriana.bootnode.repositories.NodeRepository;
+import co.edu.javeriana.bootnode.services.NodeService;
 
 @RestController
 @RequestMapping("/node")
 public class NodeController {
 	
-	private static final Logger log = LoggerFactory.getLogger(NodeController.class);
-	
 	@Autowired
-	private NodeRepository nodeRepository;
+	private NodeService nodeService;
 	
 	@PostMapping
 	public void registerNode(HttpServletRequest request, @RequestBody String publicKey) {
-		Node node = new Node();
-		node.setPublicKey(publicKey);
-		log.info("Registration Request from: " + request.getRemoteAddr()+":"+request.getRemotePort());
-		node.setIp(request.getRemoteAddr()+":"+request.getRemotePort());
-		nodeRepository.save(node);
+		nodeService.createNode(request.getRemoteAddr()+":"+request.getRemotePort(), publicKey);
 	}
 	
 	@GetMapping
 	public ResponseEntity<Iterable<Node>> getNodes(){
-		return ResponseEntity.ok(nodeRepository.findAll());
+		return ResponseEntity.ok(nodeService.getNodes());
 	}
 }
